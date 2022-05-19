35825939a5e595dfcb9b7fb368e409dc4bd060eb
IsaacLoiselle <100873727+IsaacLoiselle@users.noreply.github.com>
2022-05-19 13:40:04 -0400

    ef-IL-Creation d'un nouveau menu atelier dans fonction

--- front-page.php
+++ front-page.php
@@ -9,6 +9,11 @@
     <div class="animation__bloc"><svg width="80" height="80" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#008246"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path></svg></div>
     <div class="animation__bloc"><svg width="80" height="80" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#1679ab"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path></svg></div>
 </section>
+<h4>Les ateliers</h4>
+<?php
+    wp_nav_menu(array("menu"=>"menu_atelier",
+                    "container"=>"nav"));
+?>
 <h4>Activités en TIM</h4>
 <?php
     wp_nav_menu(array("menu"=>"menu_accueil",
