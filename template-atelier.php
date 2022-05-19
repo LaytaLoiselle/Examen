4f29eb4bd387dd5ed84cfd4e5da26bce02c3ce91
IsaacLoiselle <100873727+IsaacLoiselle@users.noreply.github.com>
2022-05-19 14:10:48 -0400

    ef-IL-Creation du template-atelier

--- template-atelier.php
+++ template-atelier.php
@@ -11,14 +11,35 @@
             <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
          <?php endif; ?>
          <?php endif ?>
-         <h3>L'endroit</h3>
-         <p><?php the_field('endroit'); ?></p>
-         <p>Date de l'événement <?php the_field('date'); ?></p>
-         <p>Heure de l'événement <?php the_field('heure'); ?></p>
-         <h3>Résumé de l'événement</h3>
-         <p><?php the_field('resume'); ?></p>
-         <h3>L'organisateur de l'événement</h3>
-         <p><?php the_field('organisateur'); ?></p>
+
+
+
+         <h3><?php the_title() ?></h3>
+
+         <h4>Description</h4>
+         <p><?php the_field('description'); ?><p>
+        
+         <h4>Animateur</h4>
+         <?php the_field('animateur'); ?>
+         <h4>Date de début</h4>
+         <?php the_field('date_de_debut'); ?>
+         <h4>Date de fin</h4>
+         <?php the_field('date_de_fin'); ?>
+         <h4>Jour de la semaine</h4>
+         <?php the_field('jour_de_la_semaine'); ?>
+         <h4>L'heure du début</h4>
+         <?php the_field('heure_de_debut'); ?>
+         <h4>L'heure de fin</h4>
+         <?php the_field('heure_de_fin'); ?>
+         <h4>La durée</h4>
+         <?php the_field('duree'); ?>
+         <h4>Le local</h4>
+         <?php the_field('local'); ?>
+         
+
+
+
+         
          <?php the_content() ?>
 
 </main>
